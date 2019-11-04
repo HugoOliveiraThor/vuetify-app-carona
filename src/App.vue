4fcb7e5fa33881def976b0a03a6932ddd3274fc6
Hugo Oliveira <hugo.oliveira@hubconexa.com>
2019-11-04 10:53:08 -0300

    adicionando a lista de produtos

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <TopToolBar />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TopToolBar from './components/TopToolBar'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    TopToolBar
   },
 
   data: () => ({
